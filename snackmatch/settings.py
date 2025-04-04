6982c2de3399bb3e047dd0152f0054568571ec9b
ajasadipura6 <ajasadipura6@gatech.edu>
2025-04-04 15:22:45 -0400

    Update settings.py

--- snackmatch/settings.py
+++ snackmatch/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'snackmatch/static/snackmatch/css/',
+    os.path.join(BASE_DIR, 'snackmatch/static'),
 ]
 
 # Default primary key field type
